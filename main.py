6b997370cc5aeda8528b8dc753647740ae7aee5d
Ansh <ansh.tiwatne@gmail.com>
2022-03-29 13:16:19 +0530

    removed nested functions

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 Tic-tac-toe solver using minimax algorithm
 """
 
-MAX: str # X or O
-MIN: str # X or O
+MAX: str  # X or O
+MIN: str  # X or O
 
 # Empty board
 grid = {
@@ -61,83 +61,85 @@ def minimax(depth: int, maximizing: bool):
 
         if maximizing:
             grid[i] = MAX
-            score = minimax(depth+1, False)
+            score = minimax(depth + 1, False)
             grid[i] = " "
             if score > best_score: best_score = score
 
         if not maximizing:
             grid[i] = MIN
-            score = minimax(depth+1, True)
+            score = minimax(depth + 1, True)
             grid[i] = " "
             if score < best_score: best_score = score
 
     return best_score
 
 
+def get_chioce():
+    """Get user's choice"""
+
+    global MAX, MIN
+
+    choice = input("\nChoose between 'X' and 'O': ").strip().upper()
+    while choice not in {"X", "O"}:
+        choice = input("Invalid choice, choose again: ").strip().upper()
+    MIN = choice
+    MAX = "O" if MIN == "X" else "X"
+
+
+def get_move():
+    """Get user's move"""
+
+    move = input("\nEnter your move: ").strip()
+    i = 0
+    while i != 2:
+        i = 0
+        if move.isnumeric():
+            move = int(move) - 1
+            i += 1
+        if grid.get(move, None) == " ":
+            i += 1
+        else:
+            move = input("Invalid move, try again: ")
+
+    return move
+
+
+def ai_move():
+    """AI's move"""
+
+    best_score = float("-inf")
+    best_move = 0
+    for i, spot in grid.items():
+        if spot != " ": continue
+        grid[i] = MAX
+        score = minimax(0, maximizing=False)
+        grid[i] = " "
+        if score > best_score: best_score, best_move = score, i
+
+    return best_move
+
+
 def main():
     """Alternate between AI and user moves"""
-
-    def get_chioce():
-        """Get user's choice"""
-        
-        global MAX, MIN
-
-        choice = input("\nChoose between 'X' and 'O': ").strip().upper()
-        while choice not in {"X", "O"}:
-            choice = input("Invalid choice, choose again: ").strip().upper()
-        MIN = choice
-        MAX = "O" if MIN == "X" else "X"
-
-    def get_move():
-        """Get user's move"""
-
-        move = input("\nEnter your move: ").strip()
-        i = 0
-        while i != 2:
-            i = 0
-            if move.isnumeric():
-                move = int(move)-1
-                i += 1
-            if grid.get(move, None) == " ":
-                i += 1
-            else:
-                move = input("Invalid move, try again: ")
-
-        return move
-
-    def ai_move():
-        """AI's move"""
-
-        best_score = float("-inf")
-        best_move = 0
-        for i, spot in grid.items():
-            if spot != " ": continue
-            grid[i] = MAX
-            score = minimax(0, maximizing=False)
-            grid[i] = " "
-            if score > best_score: best_score, best_move = score, i
-
-        return best_move
 
     get_chioce()
     print(f"\n1 | 2 | 3\n{'-'*9}\n4 | 5 | 6\n{'-'*9}\n7 | 8 | 9")
 
-    while True: # game loop
+    while True:  # game loop
 
-        if MAX == "X": # AI first
+        if MAX == "X":  # AI first
             if result() is not None: break
             grid[ai_move()] = MAX
             draw_grid()
             if result() is not None: break
             grid[get_move()] = MIN
 
-        if MIN == "X": # User first
+        if MIN == "X":  # User first
             if result() is not None: break
             grid[get_move()] = MIN
             if result() is not None: break
             grid[ai_move()] = MAX
             draw_grid()
-
 
     if result() == -1: print(f"{MIN} wins!\n")
     elif result() == +1: print(f"{MAX} wins!\n")
